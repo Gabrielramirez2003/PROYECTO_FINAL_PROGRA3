b0afa9ec5053e8c304c7682320248788e571c394
Gabrielramirez2003 <gabriel113_@outlook.com>
2026-04-30 23:57:36 -0300

    Se inicia el proyecto con las dependencias requeridas por el momento para comenzar a desarrollar de forma completa.

---
+++ src/main/java/com/example/Proyecto_Final_Progra3/ProyectoFinalProgra3Application.java
@@ -0,0 +1,13 @@
+package com.example.Proyecto_Final_Progra3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalProgra3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalProgra3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Proyecto_Final_Progra3/ProyectoFinalProgra3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Proyecto_Final_Progra3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalProgra3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
